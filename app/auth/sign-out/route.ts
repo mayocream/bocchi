9ff15180d319a55c7e82140c37d2b4dc1062044a
Mayo <mayo@linux.com>
2024-10-23 02:43:10 +0900

    remove unused routes

--- app/auth/sign-out/route.ts
+++
@@ -1,9 +0,0 @@
-import { endSession } from '@/app/lib/auth'
-import { revalidatePath } from 'next/cache'
-import { NextResponse } from 'next/server'
-
-export const GET = async () => {
-  endSession()
-  revalidatePath('/dashboard')
-  return NextResponse.redirect(process.env.NEXT_PUBLIC_BASE_URL!)
-}
