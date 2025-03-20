650c3a0bcc0fb49c15bf0510a83a50a89a855309
Mayo <mayo@linux.com>
2025-03-21 01:21:26 +0900

    add user follow, unfollow, and follower/following retrieval functionality

--- src/api/user.rs
+++ src/api/user.rs
@@ -2,9 +2,12 @@ use argon2::{
     Argon2,
     password_hash::{PasswordHash, PasswordHasher, PasswordVerifier, SaltString, rand_core::OsRng},
 };
-use sea_orm::{ActiveValue::Set, ColumnTrait, EntityTrait, QueryFilter};
+use sea_orm::{ActiveValue::Set, ColumnTrait, Condition, EntityTrait, QueryFilter, QuerySelect};
 
-use crate::{entities::user, jwt::Claims};
+use crate::{
+    entities::{follow, user},
+    jwt::Claims,
+};
 
 use super::{
     Auth, SharedAppState,
@@ -151,6 +154,123 @@ impl User for UserService {
             .map_err(|e| tonic::Status::internal(e.to_string()))?;
 
         Ok(tonic::Response::new(()))
+    }
+
+    async fn follow(
+        &self,
+        request: tonic::Request<bocchi::FollowRequest>,
+    ) -> Result<tonic::Response<()>, tonic::Status> {
+        let user_id = self.state.extract_user_id(&request)?;
+        let request = request.into_inner();
+
+        let follow = follow::ActiveModel {
+            follower_id: Set(user_id),
+            followed_id: Set(request.user_id),
+            ..Default::default()
+        };
+        follow::Entity::insert(follow)
+            .exec(&self.state.database)
+            .await
+            .map_err(|e| {
+                if e.sql_err().is_some() {
+                    tonic::Status::already_exists("Already following")
+                } else {
+                    tonic::Status::internal(e.to_string())
+                }
+            })?;
+
+        Ok(tonic::Response::new(()))
+    }
+
+    async fn unfollow(
+        &self,
+        request: tonic::Request<bocchi::UnfollowRequest>,
+    ) -> Result<tonic::Response<()>, tonic::Status> {
+        let user_id = self.state.extract_user_id(&request)?;
+        let request = request.into_inner();
+
+        let follow = follow::ActiveModel {
+            follower_id: Set(user_id),
+            followed_id: Set(request.user_id),
+            ..Default::default()
+        };
+        follow::Entity::delete(follow)
+            .exec(&self.state.database)
+            .await
+            .map_err(|e| tonic::Status::internal(e.to_string()))?;
+
+        Ok(tonic::Response::new(()))
+    }
+
+    async fn get_followers(
+        &self,
+        request: tonic::Request<bocchi::GetFollowersRequest>,
+    ) -> Result<tonic::Response<bocchi::GetFollowersResponse>, tonic::Status> {
+        let request = request.into_inner();
+        let follows = follow::Entity::find()
+            .filter(follow::Column::FollowedId.eq(request.user_id))
+            .cursor_by(follow::Column::Id)
+            .after(request.cursor.clone())
+            .first(50)
+            .all(&self.state.database)
+            .await
+            .map_err(|e| tonic::Status::internal(e.to_string()))?;
+
+        let followers = follows.iter().map(|f| f.follower_id).collect();
+
+        let response = bocchi::GetFollowersResponse {
+            followers,
+            next_cursor: follows.last().map(|f| f.id).unwrap_or_default(),
+        };
+
+        Ok(tonic::Response::new(response))
+    }
+
+    async fn get_following(
+        &self,
+        request: tonic::Request<bocchi::GetFollowingRequest>,
+    ) -> Result<tonic::Response<bocchi::GetFollowingResponse>, tonic::Status> {
+        let request = request.into_inner();
+        let follows = follow::Entity::find()
+            .filter(follow::Column::FollowerId.eq(request.user_id))
+            .cursor_by(follow::Column::Id)
+            .after(request.cursor.clone())
+            .first(50)
+            .all(&self.state.database)
+            .await
+            .map_err(|e| tonic::Status::internal(e.to_string()))?;
+
+        let following = follows.iter().map(|f| f.followed_id).collect();
+        let response = bocchi::GetFollowingResponse {
+            following,
+            next_cursor: follows.last().map(|f| f.id).unwrap_or_default(),
+        };
+        Ok(tonic::Response::new(response))
+    }
+
+    async fn search(
+        &self,
+        request: tonic::Request<bocchi::SearchUserRequest>,
+    ) -> Result<tonic::Response<bocchi::SearchUserResponse>, tonic::Status> {
+        let request = request.into_inner();
+        let users = user::Entity::find()
+            .filter(
+                Condition::any()
+                    .add(user::Column::Username.contains(request.query.clone()))
+                    .add(user::Column::Name.contains(request.query)),
+            )
+            .limit(50)
+            .all(&self.state.database)
+            .await
+            .map_err(|e| tonic::Status::internal(e.to_string()))?;
+
+        let users = users.iter().map(|u| u.id).collect();
+
+        let response = bocchi::SearchUserResponse {
+            users,
+            next_cursor: 0,
+        };
+        Ok(tonic::Response::new(response))
     }
 }
 
@@ -328,5 +448,37 @@ mod tests {
         assert_eq!(response.bio, "Bio");
         assert_eq!(response.avatar_url, "avatar_url");
         assert_eq!(response.cover_url, "cover_url");
+    }
+
+    #[tokio::test]
+    async fn test_follow() {
+        let state = setup(true).await;
+        let service = UserService::new(state.clone());
+
+        let token = state
+            .jwt
+            .encode(Claims {
+                user_id: 1,
+                exp: (chrono::Utc::now() + chrono::Duration::days(365)).timestamp() as usize,
+            })
+            .expect("Failed to encode token");
+        let request = bocchi::FollowRequest { user_id: 1 };
+        let mut request = tonic::Request::new(request);
+        request.metadata_mut().insert(
+            "authorization",
+            token.parse().expect("Failed to parse token"),
+        );
+        service.follow(request).await.expect("Failed to follow");
+
+        let request = bocchi::GetFollowersRequest {
+            user_id: 1,
+            cursor: 0,
+        };
+        let response = service
+            .get_followers(tonic::Request::new(request))
+            .await
+            .expect("Failed to get followers");
+        let response = response.get_ref();
+        assert_eq!(response.followers, vec![1]);
     }
 }
