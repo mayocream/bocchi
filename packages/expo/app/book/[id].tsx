7365c00c23103941b3f8046acd52287ae503ee78
Mayo <mayo@linux.com>
2025-03-25 20:35:48 +0900

    feat: refactor TV component to use generic data state; improve readability and maintainability

--- packages/expo/app/book/[id].tsx
+++ packages/expo/app/book/[id].tsx
@@ -6,12 +6,12 @@ import { YStack, XStack, Separator, Image, SizableText } from 'tamagui'
 
 export default function Tv() {
   const { id } = useLocalSearchParams<{ id: string }>()
-  const [googleBooksData, setGoogleBooksData] = useState<any>(null)
+  const [data, setData] = useState<any>(null)
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     googleBooks(`/volumes/${id}`)
-      .then(setGoogleBooksData)
+      .then(setData)
       .then(() => setLoading(false))
   }, [id])
 
@@ -23,56 +23,48 @@ export default function Tv() {
     <YStack fullscreen backgroundColor='$background'>
       <Stack.Screen
         options={{
-          title: googleBooksData?.volumeInfo?.title,
+          title: data?.volumeInfo?.title,
         }}
       />
 
       <XStack padding='$4' gap='$4'>
         <Image
-          source={{ uri: googleBooksData?.volumeInfo?.imageLinks?.thumbnail }}
+          source={{ uri: data?.volumeInfo?.imageLinks?.thumbnail }}
           style={{ width: 160, height: (160 * 3) / 2, borderRadius: 4 }}
         />
         <YStack gap='$2'>
           <SizableText size='$5' fontWeight='bold' width={160}>
-            {googleBooksData?.volumeInfo?.title} (
-            {googleBooksData?.volumeInfo?.publisher})
+            {data?.volumeInfo?.title} ({data?.volumeInfo?.publisher})
           </SizableText>
           <SizableText width={160}>
-            {googleBooksData?.volumeInfo?.authors?.join(', ')}
+            {data?.volumeInfo?.authors?.join(', ')}
           </SizableText>
 
           <XStack gap='$2' justifyContent='space-between'>
-            {googleBooksData?.volumeInfo?.publishedDate && (
+            {data?.volumeInfo?.publishedDate && (
               <YStack gap='$1'>
                 <SizableText>発売日</SizableText>
-                <SizableText>
-                  {googleBooksData?.volumeInfo?.publishedDate}
-                </SizableText>
+                <SizableText>{data?.volumeInfo?.publishedDate}</SizableText>
               </YStack>
             )}
 
-            {googleBooksData?.volumeInfo?.pageCount && (
+            {data?.volumeInfo?.pageCount && (
               <YStack gap='$1'>
                 <SizableText>ページ数</SizableText>
-                <SizableText>
-                  {googleBooksData?.volumeInfo?.pageCount}
-                </SizableText>
+                <SizableText>{data?.volumeInfo?.pageCount}</SizableText>
               </YStack>
             )}
           </XStack>
         </YStack>
       </XStack>
 
-      {googleBooksData?.volumeInfo?.description && (
+      {data?.volumeInfo?.description && (
         <YStack gap='$2' padding='$4'>
           <SizableText size='$4' fontWeight='bold'>
             概要
           </SizableText>
           <SizableText>
-            {googleBooksData?.volumeInfo?.description?.replace(
-              /<[^>]*>?/gm,
-              ''
-            )}
+            {data?.volumeInfo?.description?.replace(/<[^>]*>?/gm, '')}
           </SizableText>
         </YStack>
       )}
