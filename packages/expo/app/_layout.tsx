4d063ee47d5de250c606f1150e926581ee4e6447
Mayo <mayo@linux.com>
2025-04-02 22:57:39 +0900

    Add sentry

--- packages/expo/app/_layout.tsx
+++ packages/expo/app/_layout.tsx
@@ -10,6 +10,9 @@ import Loading from '@/components/loading'
 import { Platform } from 'react-native'
 import * as Font from 'expo-font'
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
+import * as Sentry from '@sentry/react-native'
+import { isRunningInExpoGo } from 'expo'
+import { useNavigationContainerRef } from 'expo-router'
 
 if (Platform.OS === 'android') {
   NavigationBar.setBackgroundColorAsync('#fff')
@@ -67,7 +70,29 @@ const AppContent = () => {
   )
 }
 
-export default function RootLayout() {
+// Sentry
+const navigationIntegration = Sentry.reactNavigationIntegration({
+  enableTimeToInitialDisplay: !isRunningInExpoGo(),
+})
+
+Sentry.init({
+  dsn: 'https://cb5e67d2708933e8a4e8eba3bc5b4d4b@o4509083941797888.ingest.us.sentry.io/4509083954970624',
+  debug: true, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
+  tracesSampleRate: 1.0, // Set tracesSampleRate to 1.0 to capture 100% of transactions for tracing. Adjusting this value in production.
+  integrations: [navigationIntegration],
+  enableNativeFramesTracking: !isRunningInExpoGo(), // Tracks slow and frozen frames in the application
+})
+
+function RootLayout() {
+  // Capture the NavigationContainer ref and register it with the integration.
+  const ref = useNavigationContainerRef()
+
+  useEffect(() => {
+    if (ref?.current) {
+      navigationIntegration.registerNavigationContainer(ref)
+    }
+  }, [ref])
+
   let tamaguiConfig = defaultConfig
   if (Platform.OS === 'web') {
     tamaguiConfig = {
@@ -98,3 +123,6 @@ export default function RootLayout() {
     </SafeAreaProvider>
   )
 }
+
+// Wrap the Root Layout route component with `Sentry.wrap` to capture gesture info and profiling data.
+export default Sentry.wrap(RootLayout)
